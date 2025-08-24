8ee20d432481f899c7f639b465535a56e22cf1bf
Evangelia Papagiannaki <evapapag2000@gmail.com>
2025-08-24 17:42:01 +0300

    Small fixes

--- src/main/java/com/example/demo/service/JobApplicationService.java
+++ src/main/java/com/example/demo/service/JobApplicationService.java
@@ -1,37 +1,50 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.JobApplicationDTO;
+import com.example.demo.dto.UserPrincipal;
 import com.example.demo.entity.JobApplication;
 import com.example.demo.repository.JobApplicationRepository;
 import com.example.demo.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JobApplicationService {
 
-    private  final JobApplicationRepository jobApplicationRepository;
+    private final JobApplicationRepository jobApplicationRepository;
     private final UserRepository userRepository;
     private final ModelMapper mapper;
 
-    public JobApplicationService(JobApplicationRepository repository, UserRepository userRepository, ModelMapper mapper){
+    public JobApplicationService(JobApplicationRepository repository, UserRepository userRepository, ModelMapper mapper) {
         this.jobApplicationRepository = repository;
         this.userRepository = userRepository;
         this.mapper = mapper;
     }
-    public JobApplicationDTO[] getJobApplications() {
-        return null;
+
+    public List<JobApplicationDTO> getJobApplications(Authentication authentication) {
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        Long userId = principal.user.getId();
+        List<JobApplication> jobApplications = this.jobApplicationRepository.findByUserId(userId);
+        List<JobApplicationDTO> applications = null;
+
+        if (!jobApplications.isEmpty()) {
+            applications = jobApplications.stream().map(this::convertToDTO).toList();
+        }
+        return applications;
     }
 
     public void createJobApplication(JobApplicationDTO jobApplicationDTO) {
         this.jobApplicationRepository.save(this.convertToEntity(jobApplicationDTO));
     }
 
-    private JobApplicationDTO convertToDTO(JobApplication jobApplication){
+    private JobApplicationDTO convertToDTO(JobApplication jobApplication) {
         return this.mapper.map(jobApplication, JobApplicationDTO.class);
     }
 
-    private JobApplication convertToEntity(JobApplicationDTO jobApplicationDTO){
+    private JobApplication convertToEntity(JobApplicationDTO jobApplicationDTO) {
         return this.mapper.map(jobApplicationDTO, JobApplication.class);
     }
 
@@ -39,7 +52,7 @@ public class JobApplicationService {
         return jobApplicationRepository;
     }
 
-    public UserRepository getUserRepository(){
+    public UserRepository getUserRepository() {
         return this.userRepository;
     }
 }
